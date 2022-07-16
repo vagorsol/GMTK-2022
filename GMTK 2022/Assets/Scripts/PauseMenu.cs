9e7cd9706dcd8690826f77a149e4788e213e9661
Audrey Yang <audrey.butter0930@gmail.com>
2022-07-15 20:51:08 -0700

    Pause menu toggle
    Known issue: Unity is not detecting escape key being entered, hence pause menu isn't actually appearing

---
+++ GMTK 2022/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,29 @@
+/**
+    Author: Audrey Yang
+    Date Created: 7/15/2022
+    Date Modified: N/A
+    Call pause menu
+*/
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool isGamePaused = false; 
+    public GameObject pauseMenuUI;
+
+    // TODO: known issue, Unity not detecting escape key being pressed
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            Debug.Log("Escape key was pressed");
+            if(isGamePaused){
+                pauseMenuUI.SetActive(false);
+                Time.timeScale = 1f;
+                isGamePaused = false;
+            } else {
+                pauseMenuUI.SetActive(true);
+                Time.timeScale = 0f;
+                isGamePaused = true;
+            }
+        }
+    }
+}
